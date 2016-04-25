69f6a8e0845b5bc6985601bc129856bc31dbe268
aiminy <jlfmssm@gmail.com>
2016-04-24 23:47:50 -0400

    remove comments

--- Run_process_vcf_files.sh
+++ Run_process_vcf_files.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
-#BSUB -n 32
-#BSUB -q general
-#BSUB -W 05:00
-#BSUB -J ANNOVAR_bash
-#BSUB -P myprojectid
-#BSUB -o %J.out
-#BSUB -e %J.err
 
-#sh ~/Script_bash/Run_process_vcf_files.sh VCF_File2BeProcessed.txt  
-
-#$annovar/annotate_variation.pl -downdb -buildver mm10 -webfrom annovar refGene $annovar/mm10db/
-
-#$annovar/annotate_variation.pl --buildver mm10 --downdb seq /scratch/projects/bbc/NGS-tools/annovar/mm10db/mm10_seq
-
-#$annovar/retrieve_seq_from_fasta.pl $annovar/mm10db/mm10_refGene.txt -seqdir $annovar/mm10db/mm10_seq -format refGene -outfile $annovar/mm10db/mm10_refGeneMrna.fa
+#Usage: sh ~/Script_bash/Run_process_vcf_files.sh VCF_File2BeProcessed.txt  
 
 while read line; do
 
@@ -23,23 +10,8 @@ fbname=$(dirname "$f" | cut -d'/' -f8)
 
 echo "$f"
 
-#sample_name=`echo "$f" | awk -F"." '{print $1}'`
 
 echo "$fbname"
-
-
-#cat<<EOF> ~/Script_bash/Run_"$fbname"_process_vcf.sh
-#!/bin/bash
-#BSUB -n 32
-#BSUB -q general
-#BSUB -W 05:00
-#BSUB -J ANNOVAR
-#BSUB -P myprojectid
-#BSUB -o %J.out
-#BSUB -e %J.err
-
-#wme $f)/$(basename "$f" | cut -d. -f1)_str.avinputc -l /scratch/projects/bbc/Project/zLi/Mutation/S2vsS6/results/mutect_output_passed.vcf.recode.vcf
-#wc -l "$f" 
 
 tail -n +52 "$f" | cut -f1,2,4,5,10-11 > /scratch/projects/bbc/Project/zLi/Mutation/"$fbname"/results/Str_processed.vcf
 
@@ -47,22 +19,5 @@ wc -l /scratch/projects/bbc/Project/zLi/Mutation/"$fbname"/results/Str_processed
 
 R --slave --args /scratch/projects/bbc/Project/zLi/Mutation/"$fbname"/results/Str_processed.vcf "$fbname" /scratch/projects/bbc/Project/zLi/Mutation/"$fbname"/results/Str_processed_vcf_freq_3.txt < ~/Script_bash/Run_process_one_vcf_files.R
 
-#cut -f1,2,10-11 "$f" | head -100 | cut -f2 | cut -d"," -f2,3,4,5 | cut -d"," -f1 | cut -d":" -f1
 
-#mutect_output_passed.vcf.recode.vcf
-
-#cut -f1-2,4- "$f" > /scratch/projects/bbc/Project/zLi/Mutation/S7vsS9/results/mutect_output_2.txt
-
-#cut -f1-2,4- "$f" > $(dirname $f)/$(basename "$f" | cut -d. -f1)_cut_c3.txt
-
-#perl /scratch/projects/bbc/NGS-tools/Convert_mut_2_vcf.pl $(dirname $f)/$(basename "$f" | cut -d. -f1)_cut_c3.txt $(dirname $f)/$(basename "$f" | cut -d. -f1)_cut_c3.vcf
-
-#$annovar/convert2annovar.pl -format vcf4old $(dirname $f)/$(basename "$f" | cut -d. -f1)_cut_c3.vcf > $(dirname $f)/$(basename "$f" | cut -d. -f1)_cut_c3_mutect.avinput
-
-#$annovar/annotate_variation.pl -out $(dirname $f)/myanno_mutect -build mm10 $(dirname $f)/$(basename "$f" | cut -d. -f1)_cut_c3_mutect.avinput $annovar/mm10db/
-
-#-out $(dirname $f)/myanno -remove -protocol refGene -operation g -nastring . -vcfinput
-
-#EOF
-#bsub -P ANNOVAR < ~/Script_bash/Run_"$fbname"_process_vcf.sh
 done < $1
